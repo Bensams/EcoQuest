7609bf47e49923d29cc96db23bbb039b473657fa
Win <erwincurato@gmail.com>
2026-08-15 09:11:39 +0800

    chore(scripts):remove legacy Windows demo helpers and use SEED_PASSWORD in wallet check

--- scripts/e2e-demo.ps1
+++
@@ -1,4 +0,0 @@
-# End-to-end automated workflow check. API and migrated local PostgreSQL must be running.
-$ErrorActionPreference = 'Stop'
-& "$PSScriptRoot/demo.ps1" @args
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
